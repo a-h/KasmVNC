5f25e44795a2044793d4287b26a87c4d807a4d90
El <eldargafiyatov@gmail.com>
2025-06-26 02:59:35 +0500

    VNC-151 Enhanced encoder builders to use a templated base class

--- common/rfb/encoders/VideoEncoderFactory.cxx
+++ common/rfb/encoders/VideoEncoderFactory.cxx
@@ -1,7 +1,7 @@
 #include "VideoEncoderFactory.h"
 
-#include "H264VAAPIEncoder.h"
 #include "H264SoftwareEncoder.h"
+#include "H264VAAPIEncoder.h"
 
 namespace rfb {
     class EncoderBuilder {
@@ -10,30 +10,34 @@ namespace rfb {
         virtual ~EncoderBuilder() = default;
     };
 
-    class H264VAAPIEncoderBuilder final : public EncoderBuilder {
+    template<typename T>
+    class H264EncoderBuilder : public EncoderBuilder {
+        FFmpeg &ffmpeg;
         int frame_rate{};
         int bit_rate{};
         SConnection *conn{};
-        H264VAAPIEncoderBuilder() = default;
+        explicit H264EncoderBuilder(FFmpeg &ffmpeg_) : ffmpeg(ffmpeg_) {}
 
     public:
-        static H264VAAPIEncoderBuilder create() {
-            return {};
+        H264EncoderBuilder() = delete;
+
+        static H264EncoderBuilder create(FFmpeg &ffmpeg) {
+            return H264EncoderBuilder{ffmpeg};
         }
 
-        H264VAAPIEncoderBuilder &with_frame_rate(int value) {
+        H264EncoderBuilder &with_frame_rate(int value) {
             frame_rate = value;
 
             return *this;
         }
 
-        H264VAAPIEncoderBuilder &with_bit_rate(int value) {
+        H264EncoderBuilder &with_bit_rate(int value) {
             bit_rate = value;
 
             return *this;
         }
 
-        H264VAAPIEncoderBuilder &with_connection(SConnection *value) {
+        H264EncoderBuilder &with_connection(SConnection *value) {
             conn = value;
 
             return *this;
@@ -43,21 +47,27 @@ namespace rfb {
             if (!conn)
                 throw std::runtime_error("Connection is required");
 
-            return new H264VAAPIEncoder(conn, frame_rate, bit_rate);
+            return new T(ffmpeg, conn, frame_rate, bit_rate);
         }
     };
 
-    Encoder *create_encoder(KasmVideoEncoders::Encoder video_encoder, SConnection *conn, uint8_t frame_rate,
-                                    uint16_t bit_rate) {
+    using H264VAAPIEncoderBuilder = H264EncoderBuilder<H264VAAPIEncoder>;
+    using H264SoftwareEncoderBuilder = H264EncoderBuilder<H264SoftwareEncoder>;
+
+    Encoder *create_encoder(KasmVideoEncoders::Encoder video_encoder, SConnection *conn, uint8_t frame_rate, uint16_t bit_rate) {
         switch (video_encoder) {
             case KasmVideoEncoders::Encoder::h264_vaapi:
-                return H264VAAPIEncoderBuilder::create()
+                return H264VAAPIEncoderBuilder::create(FFmpeg::get())
                         .with_connection(conn)
                         .with_frame_rate(frame_rate)
                         .with_bit_rate(bit_rate)
                         .build();
             default:
-                return new H264SoftwareEncoder(conn, frame_rate, bit_rate);
+                return H264SoftwareEncoderBuilder::create(FFmpeg::get())
+                        .with_connection(conn)
+                        .with_frame_rate(frame_rate)
+                        .with_bit_rate(bit_rate)
+                        .build();
         }
     }
 } // namespace rfb
