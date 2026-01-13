8c1d0eaf0dad9c3e0282345e6db50187a3a56a0f
El <eldargafiyatov@gmail.com>
2026-01-13 10:00:36 +0000

    VNC-151 Update H264 encoder configurations

--- common/rfb/encoders/EncoderConfiguration.cpp
+++ common/rfb/encoders/EncoderConfiguration.cpp
@@ -1,3 +1,20 @@
+/* Copyright (C) 2025 Kasm.  All Rights Reserved.
+*
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
 #include "EncoderConfiguration.h"
 
 namespace rfb {
@@ -31,7 +48,7 @@ namespace rfb {
             // h264_nvenc
             EncoderConfiguration{0, 51, {18, 23, 28, 39, 51}},
             // h264_software
-            EncoderConfiguration{0, 51, {9, 18, 25, 39, 51}},
+            EncoderConfiguration{1, 51, {9, 18, 25, 39, 51}},
 
             EncoderConfiguration{}
     };
